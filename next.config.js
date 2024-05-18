d0ee01b1b8ce4015c82788f057e2f427df03f28c
litury <litury@icloud.com>
2024-05-19 02:48:59 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -15,5 +15,9 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-  distDir: "build"
+  distDir: "build",
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 });
