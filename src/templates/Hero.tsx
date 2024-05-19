d08e0d98f634474783c48de75bd06e9a736bca34
litury <litury@icloud.com>
2024-05-19 21:25:23 +0500

    ✨ feat(styles): added fade-in animation to global CSS

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -7,34 +7,38 @@ import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
-const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link>
-        </li>
-        <li>
-          <Link href="/">Sign in</Link>
-        </li>
-      </NavbarTwoColumns>
-    </Section>
 
-    <Section yPadding="pt-8 pb-32">
-      <HeroOneButton
-        title={<>{'Поиск работы на автопилоте \n'}</>}
-        subtitle="для WEB-разработчиков"
-        description="Забудьте о рутине откликов доверьте это Joclic"
-        button={
-          <Link href="https://t.me/Joclic_bot">
-            <Button xl>Открыть Telegram Bot</Button>
-          </Link>
-        }
-      />
-    </Section>
-  </Background>
+const Hero = () => (
+    <Background color="bg-gray-100">
+        <Section yPadding="py-6">
+            <NavbarTwoColumns logo={<Logo xl />}>
+                <li>
+                    <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+                        GitHub
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">Sign in</Link>
+                </li>
+            </NavbarTwoColumns>
+        </Section>
+
+        <Section yPadding="pt-8 pb-32">
+            <div>
+                {/* Здесь можно добавить дополнительный контент или компоненты */}
+            </div>
+            <HeroOneButton
+                title={<>{'Ваша карьера на автопилоте'}</>}
+                subtitle="Автоматический поиск работы для разработчиков"
+                description="Отправленно 2323 откликов"
+                button={
+                    <Link href="https://t.me/Joclic_bot">
+                        <Button xl>Запустить Joclic</Button>
+                    </Link>
+                }
+            />
+        </Section>
+    </Background>
 );
 
 export { Hero };
