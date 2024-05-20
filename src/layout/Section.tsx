9843ed3a297b0aa17948ee436aecc7175f724da4
litury <litury@icloud.com>
2024-05-20 14:47:13 +0500

    ✨ feat(section): add className prop to Section component

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -5,27 +5,28 @@ type ISectionProps = {
   description?: string;
   yPadding?: string;
   children: ReactNode;
+  className?: string
 };
 
 const Section = (props: ISectionProps) => (
-  <div
-    className={`mx-auto max-w-screen-lg px-3  ${
-      props.yPadding ? props.yPadding : 'py-16'
-    }`}
-  >
-    {(props.title || props.description) && (
-      <div className="mb-12 text-center">
-        {props.title && (
-          <h2 className="text-4xl font-bold text-gray-900">{props.title}</h2>
-        )}
-        {props.description && (
-          <div className="mt-4 text-xl md:px-20">{props.description}</div>
-        )}
-      </div>
-    )}
+    <div
+        className={`mx-auto max-w-screen-lg px-3 ${props.className} ${
+            props.yPadding ? props.yPadding : 'py-16'
+        }`}
+    >
+      {(props.title || props.description) && (
+          <div className="mb-12 text-center">
+            {props.title && (
+                <h2 className="text-4xl font-bold text-gray-900">{props.title}</h2>
+            )}
+            {props.description && (
+                <div className="mt-4 text-xl md:px-20">{props.description}</div>
+            )}
+          </div>
+      )}
 
-    {props.children}
-  </div>
+      {props.children}
+    </div>
 );
 
-export { Section };
+export {Section};
