15ef43075f2649c181f06e918e28f7939b16228c
litury <litury@icloud.com>
2024-05-20 13:31:55 +0500

    ✨ feat: added typing animation and response counter in HeroOneButton component

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -1,27 +1,104 @@
-import type { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import CountUp from 'react-countup';
+import useTypewriter from 'react-typewriter-hook';
+
+
+const MagicOcean = [
+    "Frontend dev",
+    "Backend dev",
+    "Full Stack dev",
+    "Python dev",
+    "JavaScript dev",
+    "TypeScript dev",
+    "React dev",
+    "Angular dev",
+    "Vue dev"
+];
+
+let index = 0;
 
 type IHeroOneButtonProps = {
-  title: ReactNode;
-  description: string;
-  subtitle: string;
-  button: ReactNode;
+    title: ReactNode;
+    subtitle: string;
+    button: ReactNode;
 };
 
-const HeroOneButton = (props: IHeroOneButtonProps) => (
-  <header className="text-center">
+const HeroOneButton = (props: IHeroOneButtonProps) => {
+    const [responses, setResponses] = useState(2323);
+    const [previousResponses, setPreviousResponses] = useState(2323);
 
-    <h1 className="whitespace-pre-line text-5xl font-bold leading-hero text-gray-900">
-      {props.title}
-    </h1>
-    <div className="my-4 text-2xl  font-bold text-primary-500">
-      <span>{props.subtitle}</span>
-    </div>
-    <div className="mb-5">
-      <span className="mb-16 mt-4 text-2xl">{props.description}</span>
-    </div>
+    const [magicName, setMagicName] = useState(MagicOcean[index]);
+    const magicNameTyping = magicName ? useTypewriter(magicName) : '';
 
-    {props.button}
-  </header>
-);
+    const [isCursorVisible, setIsCursorVisible] = useState(false);
+    const [isTypingFinished, setIsTypingFinished] = useState(false);
 
-export { HeroOneButton };
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            index = index > 7 ? 0 : ++index;
+            setIsTypingFinished(false);
+            setMagicName(MagicOcean[index]);
+        }, 5000); // меняем текст каждые 5 секунд
+        return () => clearInterval(timer);
+    }, []);
+
+    useEffect(() => {
+        if (isTypingFinished) {
+            const timer = setInterval(() => {
+                setIsCursorVisible(v => !v);
+            }, 500); // переключаем видимость курсора каждые 0.5 секунды
+            return () => clearInterval(timer);
+        }
+        return; // Добавлено
+    }, [isTypingFinished]);
+
+
+    useEffect(() => {
+        if (magicName === magicNameTyping) {
+            setIsTypingFinished(true);
+        }
+    }, [magicName, magicNameTyping]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setPreviousResponses(responses);
+            setResponses(responses + 1);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [responses]);
+
+
+    return (
+        <header className="text-center">
+            <h1 className="whitespace-pre-line text-5xl font-bold leading-hero text-gray-900">
+                {props.title}
+            </h1>
+            <div className="my-4 text-2xl  font-bold text-primary-500">
+                <span>для </span>
+                <span>{magicNameTyping} </span>
+                <span style={{
+                    visibility: isTypingFinished && isCursorVisible ? 'visible' : 'hidden',
+                    fontFamily: 'monospace'
+                }}>
+  {' |'}
+</span>
+            </div>
+            <div className="mb-5">
+                <span className="mb-16 mt-4 ">{`Уже отправлено `}</span>
+                <span className="relative inline-block">
+                    <CountUp start={previousResponses} end={responses} duration={1.5}/>
+                    <span>{` откликов`}</span>
+                    <span
+                        style={{ top: '5px', right: '-5px' }}
+                        className="animate-pulse bg-green-500 rounded-full h-2 w-2 absolute transform translate-x-1/2 -translate-y-1/2"></span>
+                </span>
+            </div>
+
+            {props.button}
+        </header>
+    );
+};
+
+export {HeroOneButton};
