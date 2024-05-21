a567e78d21bca563e19cfe078c328a59839e31dc
litury <litury@icloud.com>
2024-05-21 15:39:00 +0500

    ✨ feat(templates): added ScrollDown component and SupportedSites component in Hero template

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,17 +1,19 @@
 import Link from 'next/link';
 
-import { Background } from '../background/Background';
-import { Button } from '../button/Button';
-import { HeroOneButton } from '../hero/HeroOneButton';
-import { Section } from '../layout/Section';
-import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
-import { Logo } from './Logo';
+import {Background} from '../background/Background';
+import {Button} from '../button/Button';
+import {HeroOneButton} from '../hero/HeroOneButton';
+import {Section} from '../layout/Section';
+import {NavbarTwoColumns} from '../navigation/NavbarTwoColumns';
+import {Logo} from './Logo';
+import {ScrollDown} from './ScrollDown';
+import {SupportedSites} from './SupportedSites';
 
 
 const Hero = () => (
-    <Background color="bg-gray-100" className="h-screen">
-        <Section yPadding="py-6">
-            <NavbarTwoColumns logo={<Logo xl />}>
+    <Background color="bg-gray-100" className="h-screen grid grid-rows-[auto, 1fr, min-content]">
+        <Section className='items-start justify-start w-full pb-0' yPadding="py-6">
+            <NavbarTwoColumns logo={<Logo xl/>}>
                 <li>
                     <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
                         GitHub
@@ -23,19 +25,27 @@ const Hero = () => (
             </NavbarTwoColumns>
         </Section>
 
-        <Section yPadding="pt-32 pb-0" >
+        <Section className='flex items-center justify-center' yPadding="pt-0 pb-0">
             <HeroOneButton
                 title={<>{'Ваша карьера на автопилоте'}</>}
                 subtitle=" для Frontend разработчиков"
 
                 button={
                     <Link href="https://t.me/Joclic_bot">
-                        <Button xl icon={true} >Запустить Joclic</Button>
+                        <Button xl icon={true}>Запустить Joclic</Button>
                     </Link>
                 }
             />
         </Section>
+
+
+            <SupportedSites/>
+
+
+        <Section className='flex items-center justify-center' yPadding="py-0">
+            <ScrollDown/>
+        </Section>
     </Background>
 );
 
-export { Hero };
+export {Hero};
