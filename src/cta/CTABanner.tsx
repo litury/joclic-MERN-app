1cd6bec846121ffeacdc84df850a2bae66c2dd5f
litury <litury@icloud.com>
2024-05-21 22:47:25 +0500

    ✨ feat(CTABanner): added 'text' prop to component

--- src/cta/CTABanner.tsx
+++ src/cta/CTABanner.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from 'react';
 type ICTABannerProps = {
   title: string;
   subtitle: string;
+  text: string;
   button: ReactNode;
 };
 
@@ -10,7 +11,8 @@ const CTABanner = (props: ICTABannerProps) => (
   <div className="flex flex-col rounded-md bg-primary-100 p-4 text-center sm:flex-row sm:items-center sm:justify-between sm:p-12 sm:text-left">
     <div className="text-2xl font-semibold">
       <div className="text-gray-900">{props.title}</div>
-      <div className="text-primary-500">{props.subtitle}</div>
+        <div className="text-gray-900">{props.subtitle}</div>
+      <div className="text-primary-500">{props.text}</div>
     </div>
 
     <div className="whitespace-no-wrap mt-3 sm:ml-2 sm:mt-0">
