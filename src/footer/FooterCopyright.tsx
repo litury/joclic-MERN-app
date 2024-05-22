854459a2f046dc796af19411174b07ba8e8eaf0c
litury <litury@icloud.com>
2024-05-22 23:02:19 +0500

    ✨ feat: update footer copyright link to Divatoz

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -3,13 +3,7 @@ import { AppConfig } from '../utils/AppConfig';
 const FooterCopyright = () => (
   <div className="footer-copyright">
     © Copyright {new Date().getFullYear()} {AppConfig.title}. <br />
-    Made with <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>.
-    {/*
-     * PLEASE READ THIS SECTION
-     * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-     * The link doesn't need to appear on every pages, one link on one page is enough.
-     * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-     */}
+    Made with <a href="https://creativedesignsguru.com">Джун на фронте</a>.
     <style jsx>
       {`
         .footer-copyright :global(a) {
