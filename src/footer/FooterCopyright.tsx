95d63fd312fe0477d2cc98b110123ad9dba7c30f
litury <litury@icloud.com>
2024-05-23 11:11:28 +0500

    ✨ feat(footer): update FooterCopyright component styling

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -1,21 +1,21 @@
-import { AppConfig } from '../utils/AppConfig';
+import {AppConfig} from '../utils/AppConfig';
 
 const FooterCopyright = () => (
-  <div className="footer-copyright">
-    © Copyright {new Date().getFullYear()} {AppConfig.title}. <br />
-    Made with <a href="https://creativedesignsguru.com">Джун на фронте</a>.
-    <style jsx>
-      {`
-        .footer-copyright :global(a) {
-          @apply text-primary-500;
-        }
+    <div className="footer-copyright">
+        © Copyright {new Date().getFullYear()} {AppConfig.title}.
+        <div> Made with <a href="https://creativedesignsguru.com">Джун на фронте</a>.</div>
+        <style jsx>
+            {`
+                .footer-copyright :global(a) {
+                    @apply text-primary-500;
+                }
 
-        .footer-copyright :global(a:hover) {
-          @apply underline;
-        }
-      `}
-    </style>
-  </div>
+                .footer-copyright :global(a:hover) {
+                    @apply underline;
+                }
+            `}
+        </style>
+    </div>
 );
 
-export { FooterCopyright };
+export {FooterCopyright};
