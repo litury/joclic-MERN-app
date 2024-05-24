a5c4a9741df78248d4254bd40289ea54a50ed0bf
litury <litury@icloud.com>
2024-05-24 08:46:29 +0500

    ✨ chore: update footer copyright message and links

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -1,9 +1,9 @@
-import {AppConfig} from '../utils/AppConfig';
+
 
 const FooterCopyright = () => (
     <div className="footer-copyright">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}.
-        <div> Made with <a href="https://creativedesignsguru.com">Джун на фронте</a>.</div>
+        © Copyright {new Date().getFullYear()} JoClic.
+        <div> Made with <a href="https://t.me/divatoz">Джун на фронте</a>.</div>
         <style jsx>
             {`
                 .footer-copyright :global(a) {
