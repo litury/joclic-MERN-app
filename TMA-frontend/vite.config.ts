7025dcfdd8713943f0f2552849c367b21e7507fc
litury <litury@icloud.com>
2024-05-24 12:16:28 +0500

    ✨ feat: added server configuration for SSL and proxy

--- TMA-frontend/vite.config.ts
+++ TMA-frontend/vite.config.ts
@@ -4,6 +4,7 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,24 @@ export default defineConfig({
   //   },
   //   host: 'tma.internal',
   // },
+
+  server: {
+    port: 443,
+    host: "0.0.0.0",
+    hmr: {
+      host: 'tg-mini-app.local',
+      port: 443,
+    },
+    https: {
+      key: fs.readFileSync('./.cert/localhost-key.pem'),
+      cert: fs.readFileSync('./.cert/localhost.pem'),
+    },
+    proxy: {
+      "/bot": {
+        target: "http://localhost:3000/bot",
+      }
+    },
+  },
   publicDir: './public',
 });
 
