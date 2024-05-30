e4f4b3d5553a8c310963c47e8e2ecdb18082ff6f
litury <litury@icloud.com>
2024-05-30 09:33:24 +0500

    ✨ feat(pusher): update pusher environment variables

--- frontend/src/components/pusher/pusher.ts
+++ frontend/src/components/pusher/pusher.ts
@@ -1,7 +1,7 @@
 import Pusher from 'pusher-js';
 
-const pusherKey = process.env.REACT_APP_PUSHER_KEY;
-const pusherCluster = process.env.REACT_APP_PUSHER_CLUSTER;
+const pusherKey = import.meta.env.VITE_PUSHER_KEY;
+const pusherCluster = import.meta.env.VITE_PUSHER_CLUSTER;
 
 if (!pusherKey || !pusherCluster) {
     throw new Error('Variables not defined');
