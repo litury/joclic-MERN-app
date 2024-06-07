3afe6d005f03602867c52fc6ec42dae80858d0b8
litury <litury@icloud.com>
2024-06-07 14:24:20 +0500

    ✨ feat(app): added new routes and controllers for auto-apply and profile

--- backend️/app.js
+++ backend️/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-const {handleAuth} = require('./head-hunter/auth/authController');
 const {getResumes} = require('./head-hunter/resumes/resumesController');
 
 const app = express();
@@ -9,17 +8,25 @@ const server = http.createServer(app);
 
 // Разрешение CORS
 app.use(cors({
-	origin: ["https://tg-mini-app.local", "https://hhcheckerfront-1ket.vercel.app", "https://litury-hhcheckerfront-4372.twc1.net"], // разрешаем домен клиента
-	credentials: true, // разрешаем куки
-	methods: ["GET", "POST", "PUT", "DELETE"], // разрешаем методы
-	allowedHeaders: ["Content-Type", "Authorization"] // разрешаем заголовки
+	origin: ["https://tg-mini-app.local", "https://hhcheckerfront-1ket.vercel.app", "https://litury-hhcheckerfront-4372.twc1.net"]
+	credentials: true,
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+const {handleAuth, checkAuth} = require('./head-hunter/auth/authController');
+const {autoApply} = require('./head-hunter/auto-apply/autoApplyController');
+const {getProfile} = require('./head-hunter/profile/profileController')
+const {applyToVacanciesController} = require('./head-hunter/timed-apply/timedApplyController');
 
 
 app.use(express.json());
 
 app.get('/redirect', handleAuth);
-app.get('/resumes', getResumes);
+app.post('/free-apply', autoApply);
+app.get('/check-auth', checkAuth);
+app.get('/profile', getProfile)
+app.post('/apply', applyToVacanciesController)
 
 
 server.listen(3001, () => {
