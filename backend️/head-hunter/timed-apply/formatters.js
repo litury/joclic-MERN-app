bdb5ebe9fd834c07075b4403a9b1b91b158e00a0
litury <litury@icloud.com>
2024-06-07 14:11:06 +0500

    add: time-apply module

---
+++ backend️/head-hunter/timed-apply/formatters.js
@@ -0,0 +1,13 @@
+async function formatVacanciesForApply(vacancies, maxApplies) {
+	const applicableVacancies = vacancies.filter(vacancy =>
+		!vacancy.archived &&
+		vacancy.apply_alternate_url !== null &&
+		!vacancy.has_test &&
+		!vacancy.employer_insider_interview
+	);
+	
+	const vacanciesToApply = applicableVacancies.slice(0, maxApplies);
+	return vacanciesToApply;
+}
+
+module.exports = {formatVacanciesForApply}
